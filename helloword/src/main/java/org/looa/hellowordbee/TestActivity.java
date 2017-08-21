29a93c896705321f175dca92269c9fdd453baa13
looa <ran_v@foxmail.com>
2017-08-21 20:09:11 +0800

    update

--- helloword/src/main/java/org/looa/hellowordbee/TestActivity.java
+++ helloword/src/main/java/org/looa/hellowordbee/TestActivity.java
@@ -2,9 +2,12 @@ package org.looa.hellowordbee;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
+
 
 /**
  * test.
@@ -30,6 +33,9 @@ public class TestActivity extends BaseActivity implements View.OnClickListener {
         if (mProxyActivity == this) content = "Plugin is running independently!";
         else content = "Plugin is running inside Nest!";
         Toast.makeText(getApplicationContext(), content, Toast.LENGTH_SHORT).show();
+        View view = LayoutInflater.from(getApplicationContext()).inflate(R.layout.view_test, null);
+        ViewGroup.LayoutParams layoutParams = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        addContentView(view, layoutParams);
     }
 
     @Override
